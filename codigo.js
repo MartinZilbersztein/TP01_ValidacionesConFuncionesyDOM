7c1fbe9a855b65cf922ba248599eccb72901561f
MartinZilber <48520155@est.ort.edu.ar>
2025-03-18 10:36:59 -0300

    Backend 99% terminado

--- codigo.js
+++ codigo.js
@@ -4,26 +4,27 @@ const notaMatematica = document.getElementById('notaMatematica');
 const notaLengua = document.getElementById('notaLengua');
 const notaEFSI = document.getElementById('notaEFSI');
 const alerta = document.getElementById('alerta');
+const promedioTexto = document.getElementById('promedio');
+const mayorNota = document.getElementById('mayorNota');
 let notasValidas = false;
 
 function onChange(id){
+    promedio.innerText = "";
     let valor = document.getElementById(id);
     const valorNumerico = parseInt(valor.value);
-    if (typeof valorNumerico == "number")
+    console.log(valorNumerico);
+    if ((isNaN(valorNumerico) || valorNumerico < notaMinima || valorNumerico > notaMaxima))
     {
-        if (valorNumerico < notaMinima || valorNumerico > notaMaxima)
-        {
-            valor.style.color = "red";
-            notasValidas = false;
-        }
-        else 
-        {
-            valor.style.color = "black"; 
-            notasValidas = true;
-        }
+        valor.style.color = "red";
+        notasValidas = false;
+        alerta.innerText = "Los valores deben ser númericos, mayores a " + notaMinima + " y menores a " + notaMaxima ;
     }
-    else alerta.innerText = "Los valores deben ser númericos";
-
+    else 
+    {
+        valor.style.color = "black"; 
+        notasValidas = true;
+        alerta.innerText = "";
+    }   
 }
 
 
@@ -31,20 +32,37 @@ formulario.addEventListener('submit', function(){
     let notaMatematica = document.getElementById('notaMatematica');
     let notaLengua = document.getElementById('notaLengua');
     let notaEFSI = document.getElementById('notaEFSI');
+    const notas = [];
+    const notasAltas=[];
+    let promedio = 0;
+    let notaAlta = 0;
     event.preventDefault();
     notaMatematica = parseInt(notaMatematica);
     notaLengua = parseInt(notaLengua);
     notaEFSI = parseInt(notaEFSI);
-    if (notaMatematica.value == "" || notaLengua.value == "" || notaEFSI.value == "")
-        alerta.innerText = "Nota vacía.";
-    else 
+    if (!notasValidas)
     {
-        alerta.innerText = "";
-        if (!notasValidas && (parseInt)(notaMatematica.value.typeOf == "number"))
-        {
-            alerta.innerText = "Notas no válidas. El valor debe ser un número y estar entre 1 y 10 inclusive."
-        }
+        alerta.innerText = "Los valores deben ser númericos, mayores a " + notaMinima + " y menores a " + notaMaxima
     }
-        
+    else
+    {
+        for (let i = 0; i < document.getElementsByClassName("nota").length; i++)
+        {
+            notas[i] = (document.getElementsByClassName("nota")[i].value)
+            promedio+=parseInt(notas[i]);
+            if (notaAlta < notas[i])
+            notaAlta = notas[i];
+        }
+        mayorNota.innerText = "La/s materia/s con mayor nota es/son "
+        for (let i = 0; i < document.getElementsByClassName("nota").length; i++)
+        {
+            if (document.getElementsByClassName("nota")[i].value == notaAlta)
+            {
+                mayorNota.innerHTML = document.getElementsByClassName("nota")[i].id;
+            }
+
+        }
+        promedioTexto.innerText = "El promedio es " + ((parseFloat)(promedio/notas.length))
+    }
 })
 
